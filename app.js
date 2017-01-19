dbf3eb13144f765b5c9d9d5bf901b75bb9aeba1e
shrutich <shruti.choudhary@intel.com>
2017-01-19 23:15:42 +0530

    changed files

---
+++ app.js
@@ -0,0 +1,21 @@
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: '7c6e2afd-50aa-495c-b8ab-eef955baaf3b', appPassword: 'dhZ6GK1MBCXdaokkhFUi9nG' }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
