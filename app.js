f871671ba07b0f4d3682620e98423c45c1a58360
shrutich <shruti.choudhary@intel.com>
2017-01-20 15:18:30 +0530

    changed in apiai

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
 
+var apiai = require('apiai');
+
+
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.PORT || 3000, function() 
@@ -16,6 +20,30 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Create bot dialogs
+// bot.dialog('/', function (session) {
+
+//     session.send("Hello World");
+// });
+
 bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+    	var app1 = apiai("43a3623db4f54a8db91c25e2023805cc");
+    	var request = app1.textRequest(msg, {
+          sessionId: "c8be547d-c34d-4135-cbc7-9d299c615454"
+      });
+      
+       
+      request.on('response', function(response) {
+        console.log(response.result.fulfillment.speech)
+         session.send(response.result.fulfillment.speech);
+
+      });
+       
+      request.on('error', function(error) {
+          console.log(error);
+      });
+      request.end();
+        
+    })
+    .onDefault(function (session) {
+        session.send("I didn't understand. Say hello to me!");
+    }));
