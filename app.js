2c8a0612aba2c25a69e9aee21aab440b2ae419ac
shrutich91 <SHRUTICHY91@GMAIL.COM>
2019-05-08 10:32:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: '7c6e2afd-50aa-495c-b8ab-eef955baaf3b', appPassword: 'dhZ6GK1MBCXdaokkhFUi9nG' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
